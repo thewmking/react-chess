0db4ecf9082b3f2a366fc9dcfd619629b89ec358
Matthew King <thewmking@gmail.com>
2020-11-11 20:55:34 -0500

    Add keys for rows & squares & pass both row index & square index to onClick

--- src/game_components/Board.js
+++ src/game_components/Board.js
@@ -2,20 +2,22 @@ import Square from './Square.js';
 import React from 'react';
 
 class Board extends React.Component {
-  renderSquare(square, i) {
+  renderSquare(square, rowIndex, index) {
     return (
       <Square
         value={square}
-        onClick={() => this.props.onClick(i)}
+        onClick={() => this.props.onClick(square, rowIndex, index)}
+        key={[rowIndex, index].toString()}
       />
     );
   }
 
   renderRow(rowIndex) {
+    var key = 'row-' + rowIndex;
     return (
-      <div className="board-row">
+      <div className="board-row" key={key}>
         {this.props.squareArray[rowIndex].map((square, index) =>
-          this.renderSquare(square, index)
+          this.renderSquare(square, rowIndex, index)
         )}
       </div>
     );
