494a916d88a90b17942ce0593f2f354ca100334d
Matthew King <thewmking@gmail.com>
2020-11-18 15:27:43 -0500

    Use let instead of var in rookPath

--- src/paths/rookPath.js
+++ src/paths/rookPath.js
@@ -1,17 +1,17 @@
 export const rookPath = (row, column, x, y, range) => {
   const squares = []
 
-  var i
+  let i
   if (x !== 0) {
-    var xAbs = Math.abs(x)
+    let xAbs = Math.abs(x)
     for (i = xAbs / x; Math.abs(i) < xAbs; x > 0 ? i++ : i--) {
       if (range.includes(row + i)) { squares.push([row + i, column])}
     }
   }
 
-  var j
+  let j
   if (y !== 0) {
-    var yAbs = Math.abs(y)
+    let yAbs = Math.abs(y)
     for (j = yAbs / y; Math.abs(j) < yAbs; y > 0 ? j++ : j--) {
       if (range.includes(column + j)) { squares.push([row, column + j])}
     }
